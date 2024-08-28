f4199acd306e9aba298eec522942f585853ce9b2
IzadoraFernandes <izadorasilvams@gmail.com>
2024-08-28 17:43:07 -0300

    arrumando as permissões da viewsets

--- books/viewsets.py
+++ books/viewsets.py
@@ -12,22 +12,22 @@ class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
     search_fields = ('nome_categoria',)
-    #http_method_names = ['get', 'head', 'options']
+    http_method_names = ['get', 'head', 'options']
     
 class EditoraViewSet(ModelViewSet):
     queryset = Editora.objects.all()
     serializer_class = EditoraSerializer
     search_fields = ('nome_editora',)
-    #http_method_names = ['get', 'head', 'options']
+    http_method_names = ['get', 'head', 'options']
     
 class AutorViewSet(ModelViewSet):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
     search_fields = ('nome_autor',)
-    #http_method_names = ['get', 'head', 'options']
+    http_method_names = ['get', 'head', 'options']
     
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
     search_fields = ('nome_livro',)
-    #http_method_names = ['get', 'head', 'options']
+    http_method_names = ['get', 'head', 'options']
